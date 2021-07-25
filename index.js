7ebc88a686d9d4821a6df15b1819333992ca749e
Vikram GK <gkvikram95@gmail.com>
2021-07-25 23:48:13 +0800

    part3.4

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    },
+    {
+        "id": 5,
+        "name": "hey",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -47,15 +52,22 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
-    
+
     if (!person) {
-        res.sendStatus(404)
+        res.sendStatus(404).end
     } else {
         res.json(person)
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    console.log('del', persons)
+    res.sendStatus(404).end
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
