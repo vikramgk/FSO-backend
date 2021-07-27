b684025b1f7605b9a3d9426bf87dbade4d5e08a6
Vikram GK <gkvikram95@gmail.com>
2021-07-27 11:48:38 +0800

    part3.8 done

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const app = express()
 // middleware
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 // constants
 
